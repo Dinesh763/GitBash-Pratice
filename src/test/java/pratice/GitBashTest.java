01de4eb68468d7e2df0ba81f7895d7499d845b13
Dinesh763 <dksubudhy777@gmail.com>
2022-07-25 12:20:06 +0530

    second commit

--- src/test/java/pratice/GitBashTest.java
+++ src/test/java/pratice/GitBashTest.java
@@ -4,5 +4,6 @@ public class GitBashTest {
 public static void main(String[] args) {
 	System.out.println("GitBash is here");
 	System.out.println("Gitbash done");
+	System.out.println("Cross Check");
 }
 }
